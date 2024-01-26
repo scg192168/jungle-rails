d75110c4cb2c5b110ad742bee52410ae68ee5d37
scg192168 <scghoshbc@gmail.com>
2024-01-26 10:43:54 -0800

    Category implemented

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,30 @@
-class CategoriesController < ApplicationController
+class Admin::CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
+
+  def index
+    @categories = Category.all
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to admin_categories_path, notice: 'Category was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :description)
+  end
 end
